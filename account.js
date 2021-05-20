1ad9306d323d9cfcdc32cd24c995edcdd531d006
Jenelle Schilcher <jschilcher8@gmail.com>
2021-05-20 17:17:45 -0500

    Add importing

--- account.js
+++ account.js
@@ -4,21 +4,7 @@ const prompt = require("prompt-sync")();
 
 let userPin = 1234;
 
-const enteredPin = prompt()
-
-function validateUserPin(enteredPin){
-    console.log("Please enter pin")
-    while(parseInt(enteredPin) != userPin){
-        prompt("Please enter correct pin");
-    }    
-    if(parseInt(enteredPin) = userPin){
-        console.log("User Validated.")
-        break;
-    }
-}
-
 let bankAccountBalance = 4000;
 
 module.exports.pin = userPin;
-module.exports.validate = validateUserPin;
 module.exports.balance = bankAccountBalance;
